de12c3f92e6a524bf9ed61f8df32e765f27992ad
Ahmat Badali <70948233+ahmatbadali@users.noreply.github.com>
2026-05-05 14:05:59 +0800

    Update settings.js

--- ForUCantikk<3/jscp/settings.js
+++ ForUCantikk<3/jscp/settings.js
@@ -25,8 +25,10 @@ let settings = {
         // --- Lembar 2 (data-page="1") ---
         { 
             image: './image/photos/b2.png', // Menggantikan fjr3qh.png (Index 2)
-            content: 'Happy Birthday Adek Cantik 💗 <br>by Mas dali' 
+            content: 'Happy Birthday Adek Cantik 💗<br>by Mas dali' 
+            document.querySelector('.content-text').innerHTML = data.content;
         },
+
         { 
             image: './image/photos/b3.png', // Menggantikan 8u5tf7.png (Index 3)
         },
@@ -368,7 +370,8 @@ function initializeDefaultSettings() {
             // --- Lembar 2 (data-page="1") ---
             { 
                 image: './image/photos/b2.png', // Menggantikan fjr3qh.png (Index 2)
-                content: 'Happy Birthday Adek Cantik 💗 <br>by Mas dali' 
+                content: 'Happy Birthday Adek Cantik 💗<br>by Mas dali' 
+                document.querySelector('.content-text').innerHTML = data.content;
             },
             { 
                 image: './image/photos/b3.png', // Menggantikan 8u5tf7.png (Index 3)
